353e051cb8032d2a65f5e01c01fcc7d5f1265f12
ZekeriyaOtumlu <zekyotumlu@hotmail.com>
2023-01-05 23:21:46 +0000

    Initiation of codes

--- src/app.py
+++ src/app.py
@@ -9,6 +9,10 @@ from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from admin import setup_admin
 from models import db, User
+import requests
+import json
+
+#print(response_API.status_code)
 #from models import Person
 
 app = Flask(__name__)
@@ -36,14 +40,78 @@ def handle_invalid_usage(error):
 def sitemap():
     return generate_sitemap(app)
 
-@app.route('/user', methods=['GET'])
-def handle_hello():
+# @app.route('/user', methods=['GET'])
+# def handle_hello():
 
-    response_body = {
-        "msg": "Hello, this is your GET /user response "
-    }
+#     response_body = {
+#         "msg": "Hello, this is your GET /user response "
+#     }
 
-    return jsonify(response_body), 200
+#     return jsonify(response_body), 200
+
+@app.route('/people', methods=['GET'])
+def get_all_people():
+    response_API = requests.get('https://swapi.dev/api/people')
+    print(response_API.text)
+
+    # response_body = {
+    #     "msg": "Hello, this is your GET /user response "
+    # }
+
+    return response_API.json()["results"], 200
+    response_API.status_code != 200
+    return "", 404
+
+@app.route('/planets', methods=['GET'])
+def get_all_planets():
+    response_API = requests.get('https://swapi.dev/api/planets/')
+    print(response_API.text)
+
+    return (response_API.json()), 200
+ 
+@app.route('/people/<people_id>', methods=['GET'])
+def get_people(people_id):
+    response_API = requests.get('https://swapi.dev/api/people/'+ people_id)
+    print(response_API.text)
+
+    return (response_API.json()), 200
+ 
+
+@app.route('/planets/<planet_id>', methods=['GET'])
+def get_planet(planet_id):
+    response_API = requests.get('https://swapi.dev/api/planets/'+ planet_id)
+    print(response_API.text)
+
+    return (response_API.json()), 200
+
+
+
+#From DB -- models.py file
+@app.route('/users', methods=['GET'])
+def get_all_users():
+
+    users_query = User.query.all()
+    all_users = list(map(lambda x: x.serialize(), users_query))
+    print(all_users)
+    return all_users, 200
+
+
+@app.route('/favorite/planet/<planet_id>/<user_id>', methods=['POST'])
+def get_all_favorites(planet_id, user_id):
+    
+    user1 = User.query.get(user_id)
+    if user1 is None:
+        
+        user1 = User(id= user_id, favorite_planets=json.dumps([planet_id]), favorite_people=json.dumps([]))
+        db.session.add(user1)
+        db.session.commit()
+        return "ok", 200
+
+    fav_planets = json.loads(user1.favorite_planets) # change str to list
+    fav_planets.append(planet_id)
+    user1.favourite_planets = json.dumps(fav_planets) #list to str
+    db.session.commit()
+    return "ok2", 200
 
 # this only runs if `$ python src/app.py` is executed
 if __name__ == '__main__':
